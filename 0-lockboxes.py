9eb4070f31adbdeb0a3bf7f989064e22075da356
GasimovNijat <nijatt.gasimov@gmail.com>
2024-03-15 14:58:25 +0400

    box task

---
+++ 0-lockboxes.py
@@ -0,0 +1,20 @@
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+opened_box=[]
+keys_l = []
+def canUnlockAll(boxes):
+    opened_box.append(boxes[0])
+    for i in range(100):
+        for box in opened_box:
+            for keys in box:
+                if(keys not in keys_l):
+                    keys_l.append(keys)
+        for key in keys_l:
+            opened_box.append(boxes[key])
+    if(0 in keys_l):
+        keys_l.remove(0)
+    if(len(keys_l) == len(boxes)-1):
+        return True
+    else:
+        return False
+print(canUnlockAll(boxes))
+
