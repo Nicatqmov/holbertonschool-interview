928691f335d2019f98592aa4bbb7adfd03dad328
GasimovNijat <nijatt.gasimov@gmail.com>
2024-03-15 15:50:02 +0400

    box task

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,38 +1,20 @@
 #!/usr/bin/python3
-"""
-Module for lockboxes.
-"""
-
-opened_box = []
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+opened_box=[]
 keys_l = []
-
-from typing import List
-
-def canUnlockAll(boxes: List[List[int]]) -> bool:
-    """
-    Determines if all the boxes can be opened.
-
-    Args:
-        boxes (List[List[int]]): A list of lists where each sublist contains the keys inside a box.
-
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
-    if not boxes or len(boxes) == 1:
-        return True # Output: True
-
+def canUnlockAll(boxes):
     opened_box.append(boxes[0])
-    for i in range(1000):
+    for i in range(100):
         for box in opened_box:
             for keys in box:
-                if keys not in keys_l:
+                if(keys not in keys_l):
                     keys_l.append(keys)
         for key in keys_l:
             opened_box.append(boxes[key])
-    if 0 in keys_l:
+    if(0 in keys_l):
         keys_l.remove(0)
-    if len(keys_l) == len(boxes) - 1:
-        return True # Output: True
+    if(len(keys_l) == len(boxes)-1):
+        return True
     else:
-        return False # Output: False
-
+        return False
+print(canUnlockAll(boxes))
