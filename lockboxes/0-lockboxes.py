ab16d1708f936b94b0b190d26e1f2835821f8ef5
GasimovNijat <nijatt.gasimov@gmail.com>
2024-03-15 15:25:08 +0400

    box task

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 opened_box=[]
 keys_l = []
 def canUnlockAll(boxes):
@@ -17,4 +16,3 @@ def canUnlockAll(boxes):
         return True
     else:
         return False
-print(canUnlockAll(boxes))
